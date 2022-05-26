3cd9dbc4b84051b607648b58f3bdee9dadaace29
iamshs <mdsolayman998@gmail.com>
2022-05-26 17:52:55 +0600

    final commit

--- index.js
+++ index.js
@@ -167,6 +167,14 @@ async function run() {
       res.send({ admin: isAdmin });
     });
 
+
+    app.delete('/user/:id' , async(req,res)=>{
+      const id = req.params.id
+      const query = { _id:ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     //getting all reviews
     app.get("/review", async (req, res) => {
       const query = {};
